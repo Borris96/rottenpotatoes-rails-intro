0df7ee4a37223a6f53fcf093bb4e701eea2919ea
EC2 Default User <ec2-user@ip-172-31-5-109.us-east-2.compute.internal>
2019-02-11 21:35:40 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.ratings
     ratings = @all_ratings
     ratings = params[:ratings].keys if params.keys.include? "ratings"
-    @movies = Movie.where(:rating=>rating).order(sort_by)
+    @movies = Movie.where(:rating => rating).order(sort_by)
   end 
 
   def new
